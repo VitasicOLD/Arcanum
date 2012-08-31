60fcddb8a7393ab9068fe97a067f082f7198feeb
Vitasic <vitasic-pokataev@yandex.ru>
2012-08-31 19:18:16 +0400

    [Core] Improved alcohol handling!

--- sql/Arcanum/characters/Improved alcohol handling, weeeeeeeeeeeeeeeee.sql
+++ sql/Arcanum/characters/Improved alcohol handling, weeeeeeeeeeeeeeeee.sql
@@ -1,2 +1,2 @@
-UPDATE characters SET drunk = (drunk / 256) & 0xFF;
-ALTER TABLE characters CHANGE drunk drunk tinyint(3) unsigned NOT NULL DEFAULT '0';
+UPDATE characters SET drunk = (drunk * 256) & 0xFF;
+ALTER TABLE characters CHANGE drunk drunk smallint(5) unsigned NOT NULL DEFAULT '0';
