52c9a9eadc13c414e7d783b52210709511b9928d
Vitasic <vitasic-pokataev@yandex.ru>
2012-09-08 22:27:33 +0400

    [TC] SQL/Achievements: Fixed achievement "Wrecking Ball"

--- sql/Arcanum/world/[TC]_Fix_part_1.sql
+++ sql/Arcanum/world/[TC]_Fix_part_1.sql
@@ -54,3 +54,14 @@ UPDATE `script_texts` SET `content_loc8` = 'Я выиграю свою свободу и оставлю это
 UPDATE `script_texts` SET `content_loc8` = 'Нет спасения' WHERE `entry` = -1609080;
 UPDATE `script_texts` SET `content_loc8` = 'Запомни этот день, $n, ибо это тот день, когда ты будешь повержен!' WHERE `entry` = -1609081;
 UPDATE `script_texts` SET `content_loc8` = 'Не смеши меня.' WHERE `entry` = -1609083;
+
+-- Criteria data for achievement 'Wrecking Ball'
+DELETE FROM `disables` WHERE `sourceType`=4 AND `entry` IN (3368, 3369, 3370, 12578, 7623, 3371);
+DELETE FROM `achievement_criteria_data` WHERE `criteria_id` IN (3368, 3369, 3370, 12578, 7623, 3371);
+INSERT INTO `achievement_criteria_data`(`criteria_id`,`type`,`value1`,`value2`,`ScriptName`) VALUES
+(3368,2,0,0,''),
+(3369,2,0,0,''),
+(3370,2,0,0,''),
+(12578,2,0,0,''),
+(7623,2,0,0,''),
+(3371,2,0,0,'');
