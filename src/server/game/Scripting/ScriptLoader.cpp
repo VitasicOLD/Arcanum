7990669c44fba00314260e419e80878abdf11538
Vitasic <vitasic-pokataev@yandex.ru>
2012-09-25 21:47:39 +0400

    Откат к старой системе чат цензуры + её обновление

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -24,7 +24,7 @@ void AddSC_item_custom_summon();
 void AddSC_vip_commandscript();
 void AddSC_gm_monitoring();
 void AddSC_boss_kill_history();
-void AddSC_reklama();
+void AddSC_System_Censure();
 void AddSC_transmogrify_script();
 void AddSC_npc_title();
 void AddSC_npc_arena_setup();
@@ -1279,7 +1279,7 @@ void AddCustomScripts()
     AddSC_vip_commandscript();
     AddSC_gm_monitoring();
     AddSC_boss_kill_history();
-    AddSC_reklama();
+    AddSC_System_Censure();
     AddSC_transmogrify_script();
     AddSC_npc_title();
     AddSC_npc_arena_setup();
